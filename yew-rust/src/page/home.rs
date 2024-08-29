cb8f7097c3515f06e3a526fb05f1d1b7bf2694cf
lidm0707 <73036284+lidm0707@users.noreply.github.com>
2024-08-29 23:32:28 +0700

    test

--- yew-rust/src/page/home.rs
+++ yew-rust/src/page/home.rs
@@ -1,12 +1,12 @@
 use crate::components::Navigation;
 use yew::prelude::*;
-
+ 
 #[function_component(Home)]
 pub fn home() -> Html {
     html! {
 
         <body>
-        <div class = "container-nav"><Navigation/></div>
+        <Navigation/>
             <main>
                 <div class = "box-content">{"HELLO RUST"}</div>
                 <div class = "box-content">{"HELLO RUST"}</div>
